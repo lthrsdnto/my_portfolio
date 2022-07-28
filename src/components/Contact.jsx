c5e017e5d5089f0dfa132b504b74d0ef8d6867bf
Luther Sedanto <109575711+muhay007@users.noreply.github.com>
2022-07-28 19:45:01 +0800

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -21,7 +21,7 @@ function Contact() {
                     "_blank"
                   )
                 } className="mt-4 cursor-pointer inline-flex items-center py-2 px-4 text-md font-medium text-center bg-[#0A1929] hover:text-[#64FFDA] rounded-2xl hover:scale-105 hover:rounded-none duration-200 ease-in-out">
-                Resume
+                Download my CV
               </div>
             </div>
           </div>
