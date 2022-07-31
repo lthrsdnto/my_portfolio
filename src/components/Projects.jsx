16840b0a7b5bc5160f9f5aea1a0a6889d6293b7f
Luther Sedanto <109575711+muhay007@users.noreply.github.com>
2022-07-31 20:20:51 +0800

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import splashy from "../assets/splashy.png";
 import imxt from "../assets/imxt.png";
+import tea from "../assets/tea.png";
 import { FaGithub } from "react-icons/fa";
 import { BiRightArrow } from "react-icons/bi";
 import { motion } from "framer-motion";
@@ -35,13 +36,12 @@ function Projects({ textEnter, textLeave }) {
               <span className="bg-[#0A1929] hover:text-[#64FFDA] text-xs font-semibold mr-2 px-2.5 py-0.5 rounded">
                 AXIOS
               </span>
-          
             </div>
-            <h3 className="text-center text-xl py-4 font-bold">Splashy.</h3>
+            <h3 className="text-center text-xl py-4 font-bold">splashy.</h3>
             <p className="text-left text-md whitespace-normal h-24 overflow-auto">
-              Image search web app with the use of Unsplash API. This project
-              really helped me understand how http requests, fetching data and
-              displaying them on the browser.
+              This project uses the Unsplash API to create an image search web
+              app. Helped me understand how http requests, data retrieval, and
+              showing them in the browser.
             </p>
             <br />
             <div className="flex justify-between py-4">
@@ -85,10 +85,11 @@ function Projects({ textEnter, textLeave }) {
             </div>
             <h3 className="text-center text-xl py-4 font-bold">imxt_</h3>
             <p className="text-left text-md whitespace-normal h-24 overflow-auto">
-              OCR web app that can convert an image to text. I created this
-              project because sometimes i needed a specific code to run that I
-              watched from yt but there is no repo, so i just screenshot it then
-              convert, copy it instead of typing it.
+              OCR online application that can text-to-image conversion. I
+              produced this project since occasionally I wanted to run a
+              particular code that I YT video was watched, but there was no
+              repository, so I just screenshot it. rather than typing it,
+              convert it and copy it.
             </p>
             <br />
             <div className="flex justify-between py-4">
@@ -107,6 +108,46 @@ function Projects({ textEnter, textLeave }) {
                 onClick={() =>
                   window.open("https://github.com/muhay007/imxt", "_blank")
                 }
+              />
+            </div>
+          </div>
+        </div>
+        <div className="bg-gray-400 rounded-2xl bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border-indigo-900 border-2 dark:border-none">
+          <img
+            src={tea}
+            alt=""
+            className="object-fit w-full h-3/5 object-center py-4 px-6  duration-200 ease-linear"
+          />
+          <div className=" dark:text-white text-gray-700 w-4/5 mx-auto ">
+            <div className="flex flex-wrap text-white gap-2">
+              <span className="bg-[#0A1929]  hover:text-[#64FFDA] text-xs font-semibold mr-2 px-2.5 py-0.5 rounded">
+                PHP
+              </span>
+              <span className="bg-[#0A1929] hover:text-[#64FFDA] text-xs font-semibold mr-2 px-2.5 py-0.5 rounded">
+                MYSQL
+              </span>
+              <span className="bg-[#0A1929] hover:text-[#64FFDA] text-xs font-semibold mr-2 px-2.5 py-0.5 rounded">
+                BOOTSTRAP
+              </span>
+            </div>
+            <h3 className="text-center text-xl py-4 font-bold">_</h3>
+            <p className="text-left text-md whitespace-normal h-24 overflow-auto">
+              Development of Supply Chain with Stock Monitoring for TEA101
+              (Capstone)
+            </p>
+            <br />
+            <div className="flex justify-between py-4">
+              <div
+                // onClick={() => window.open("", "_blank")}
+                className="line-through cursor-pointer inline-flex items-center py-2 px-4 text-md font-medium text-center bg-[#0A1929] text-white hover:text-[#64FFDA] rounded-2xl hover:scale-105 hover:rounded-none duration-200 ease-in-out"
+              >
+                LIVE
+                <BiRightArrow className="ml-2" />
+              </div>
+
+              <FaGithub
+                className="line-through w-10 h-10 cursor-pointer hover:text-[#64FFDA] duration-200 ease-in-out"
+                // onClick={() => window.open("", "_blank")}
               />
             </div>
           </div>
