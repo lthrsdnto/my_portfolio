01430658593ef484907ed3a4e7e62ac127dec331
Luther Sedanto <109575711+muhay007@users.noreply.github.com>
2022-07-31 20:20:33 +0800

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "../App.css";
-import giphy from "../assets/giphy.gif";
+import Spline from "@splinetool/react-spline";
 
 function FrontOfCard() {
   return (
     <div className="absolute inset-0 w-full h-full bg-white transition-all duration-100 delay-200 z-20 hover:opacity-0 font-garamond text-center text-gray-700">
       <div className="flex justify-between pt-6 px-4">
-        <h1 className="font-bold text-lg">0951 755 1940</h1>
+        <h1 className="font-bold text-lg">0000 000 0000 </h1>
         <div className="flex flex-wrap flex-col">
           <h1 className="text-lg font-bold">DAY {"&"} NIGHT</h1>
           <p className="text-xs font-medium">{"< />"} ALL THE WAY</p>
@@ -17,7 +17,8 @@ function FrontOfCard() {
         <h2 className="text-lg">WEB DEVELOPER</h2>
       </div>
       <p className="text-xs font-medium p-4">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, autem.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit,
+        necessitatibus?
       </p>
     </div>
   );
@@ -25,23 +26,23 @@ function FrontOfCard() {
 
 function BackOfCard() {
   return (
-    <div className="absolute inset-0 w-full h-full flex justify-center items-center bg-black transition-all z-10 card-back">
-      <img src={giphy} alt="" />
-    </div>
+    <div className="absolute inset-0 w-full h-full flex justify-center items-center bg-black transition-all z-10 card-back"></div>
   );
 }
 
 function Hero() {
   return (
-    <div className=" max-w-screen-lg mx-auto p-4">
+    <div className=" max-w-screen-lg mx-auto">
       <div className="flex flex-wrap md:gap-4 py-8">
-        <div className="md:basis-[45%] basis-full flex flex-1 h-[400px] justify-center items-center">
+        <div className="md:basis-[45%] basis-full flex h-[400px] justify-center items-center">
           <div className="relative w-3/4 h-1/2 text-white overflow-hidden cursor-pointer transition-all duration-700 filter drop-shadow-lg card ">
             <FrontOfCard />
             <BackOfCard />
           </div>
         </div>
-        <div className="md:basis-[45%] basis-full flex flex-1 h-[400px] bg-bighead bg-cover bg-center filter rounded-full duration-200 ease-linear"></div>
+        <div className="md:basis-[45%] basis-full hidden md:block ">
+          <Spline scene="https://prod.spline.design/I02DuoS6NB58TCc0/scene.splinecode" />
+        </div>
       </div>
     </div>
   );
