394e8c94c24815534fea06808588166d1091ecc3
Luther Sedanto <109575711+muhay007@users.noreply.github.com>
2022-08-02 16:55:53 +0800

    Update Form.jsx

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -92,7 +92,7 @@ function Form() {
         <button
           type="submit"
           value="Send"
-          className="text-white inline-flex items-center py-2 px-4 text-md font-medium text-center bg-[#0A1929] hover:text-[#64FFDA] rounded-2xl hover:scale-105 hover:rounded-none duration-200 ease-in-out"
+          className="text-white inline-flex items-center py-2 px-4 text-sm font-medium text-center bg-[#0A1929] hover:text-[#64FFDA] rounded-2xl hover:scale-105 hover:rounded-none duration-200 ease-in-out"
         >
           Submit
         </button>
