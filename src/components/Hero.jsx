3504a5266a94769d5f1d5fc9611a5bbb0a270e94
Luther Sedanto <109575711+muhay007@users.noreply.github.com>
2022-08-02 17:34:40 +0800

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -26,36 +26,36 @@ function FrontOfCard() {
 
 function BackOfCard() {
   return (
-    <div className="absolute w-full h-full flex justify-center items-center bg-white transition-all z-10 card-back text-gray-700">
-    <div className="p-4 overflow-y-auto">
-    <p className="text-sm overflow-y-auto">Hey, you. You're finally awake.
+    <div className="absolute w-full h-full flex justify-center items-center bg-white transition-all z-10 card-back text-gray-700 overflow-auto">
+    <div className="p-4 overflow-auto">
+    <p>Hey, you. Youre finally awake.
 You were trying to cross the border, right?
 Walked right into that Imperial ambush, same as us, and that thief over there.
 Damn you Stormcloaks. Skyrim was fine until you came along.
-Empire was nice and lazy. If they hadn't been looking for you,
-I could've stolen that horse and be halfway to Hammerfell. You there.
-You and me - we shouldn't be here. It's these Stormcloaks the Empire wants.
-We're all brothers and sisters in binds now, thief. Shut up back there!
-And what's wrong with him, huh? Watch your tongue.
-You're speaking to Ulfric Stormcloak, the true High King. Ulfric?
-The Jarl of Windhelm? You're the leader of the rebellion.
-But if they've captured you... Oh gods, where are they taking us?
-I don't know where we're going, but Sovngarde awaits.
-No, this can't be happening. This isn't happening.
+Empire was nice and lazy. If they hadnt been looking for you,
+I couldve stolen that horse and be halfway to Hammerfell. You there.
+You and me - we shouldnt be here. Its these Stormcloaks the Empire wants.
+Were all brothers and sisters in binds now, thief. Shut up back there!
+And whats wrong with him, huh? Watch your tongue.
+Youre speaking to Ulfric Stormcloak, the true High King. Ulfric?
+The Jarl of Windhelm? Youre the leader of the rebellion.
+But if theyve captured you... Oh gods, where are they taking us?
+I dont know where were going, but Sovngarde awaits.
+No, this cant be happening. This isnt happening.
 Hey, what village are you from, horse thief? Why do you care?
-A Nord's last thoughts should be of home. Rorikstead. I'm...
-I'm from Rorikstead.
+A Nords last thoughts should be of home. Rorikstead. Im...
+Im from Rorikstead.
 
 ...looks like the Thalmor are with him.
 
-General Tullius, sir. The headsman is waiting. Good. Let's get this over with!
+General Tullius, sir. The headsman is waiting. Good. Lets get this over with!
 Shor, Mara, Dibella, Kynareth, Akatosh. Divines, please help me. Look at him.
 General Tullius the Military Governor.
 And it looks like the Thalmor are with him.
 Damn elves. I bet they had something to do with this.
 
-Why are we stopping? Why do you think? End of the line. Let's go.
-Shouldn't keep the gods waiting for us.
+Why are we stopping? Why do you think? End of the line. Lets go.
+Shouldnt keep the gods waiting for us.
 Enough with procrastination. Go back to coding.</p>
     
     </div>
