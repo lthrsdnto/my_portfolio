823ada73678241b9787f28facc45b9cb6f062b19
Luther Sedanto <109575711+muhay007@users.noreply.github.com>
2022-09-03 20:19:21 +0800

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,6 +4,7 @@ import imxt from "../assets/imxt.png";
 import tea from "../assets/tea.png";
 import netlok from "../assets/netlok.png";
 import brms from "../assets/brms.png";
+import todos from "../assets/todos.png";
 import { FaGithub } from "react-icons/fa";
 import { BiRightArrow } from "react-icons/bi";
 import { motion } from "framer-motion";
@@ -11,7 +12,7 @@ import { motion } from "framer-motion";
 function Projects({ textEnter, textLeave }) {
   return (
     <div className="max-w-screen-lg mx-auto py-8 my-8 font-poppins">
-      <div className="dark:text-white text-4xl font-bold text-black pb-4 text-center flex items-center justify-center">
+      <div className="dark:text-white md:text-4xl text-2xl font-bold text-black pb-4 text-center flex items-center justify-center">
         <motion.h1
           onMouseEnter={textEnter}
           onMouseLeave={textLeave}
@@ -21,7 +22,7 @@ function Projects({ textEnter, textLeave }) {
         </motion.h1>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4 place-items-center p-4 mx-auto">
-              <div className="bg-gray-400 rounded-2xl bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border-indigo-900 border-2 dark:border-none w-full">
+        <div className="bg-gray-400 rounded-2xl bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border-indigo-900 border-2 dark:border-none md:w-full w-5/6">
           <img
             src={splashy}
             alt=""
@@ -41,12 +42,14 @@ function Projects({ textEnter, textLeave }) {
               <span className="bg-[#0A1929] hover:text-[#64FFDA] text-xs font-semibold mr-2 px-2.5 py-0.5 rounded">
                 UNSPLASH_API
               </span>
-                <span className="bg-[#0A1929] hover:text-[#64FFDA] text-xs font-semibold mr-2 px-2.5 py-0.5 rounded">
+              <span className="bg-[#0A1929] hover:text-[#64FFDA] text-xs font-semibold mr-2 px-2.5 py-0.5 rounded">
                 FS_LIGHTBOX
               </span>
             </div>
-            <h3 className="text-center text-xl py-4 font-bold">splashy</h3>
-            <p className="text-left text-md whitespace-normal h-24 overflow-auto scrollbar">
+            <h3 className="text-center md:text-xl text-md py-4 font-bold">
+              splashy
+            </h3>
+            <p className="text-justify md:text-base text-sm whitespace-normal md:h-24 h-16 overflow-auto scrollbar px-2">
               This project uses the Unsplash API to create an image search web
               app. Helped me understand how http requests, data retrieval, and
               showing them in the browser.
@@ -57,14 +60,14 @@ function Projects({ textEnter, textLeave }) {
                 onClick={() =>
                   window.open("https://splashy-alpha.vercel.app/", "_blank")
                 }
-                className="cursor-pointer inline-flex items-center py-2 px-4 text-md font-medium text-center bg-[#0A1929] text-white hover:text-[#64FFDA] rounded-2xl hover:scale-105 hover:rounded-none duration-200 ease-in-out"
+                className="cursor-pointer inline-flex items-center py-2 px-4 md:text-sm text-xs font-medium text-center bg-[#0A1929] text-white hover:text-[#64FFDA] rounded-2xl hover:scale-105 hover:rounded-none duration-200 ease-in-out"
               >
                 LIVE
                 <BiRightArrow className="ml-2" />
               </div>
 
               <FaGithub
-                className="w-10 h-10 cursor-pointer hover:text-[#64FFDA] duration-200 ease-in-out"
+                className="md:w-10 md:h-10 w-7 h-7 cursor-pointer hover:text-[#64FFDA] duration-200 ease-in-out"
                 onClick={() =>
                   window.open("https://github.com/muhay007/splashy", "_blank")
                 }
@@ -72,7 +75,7 @@ function Projects({ textEnter, textLeave }) {
             </div>
           </div>
         </div>
-        <div className="bg-gray-400 rounded-2xl bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border-indigo-900 border-2 dark:border-none w-full">
+        <div className="bg-gray-400 rounded-2xl bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border-indigo-900 border-2 dark:border-none md:w-full w-5/6">
           <img
             src={imxt}
             alt=""
@@ -93,10 +96,12 @@ function Projects({ textEnter, textLeave }) {
                 TESSERACT_API
               </span>
             </div>
-            <h3 className="text-center text-xl py-4 font-bold">imxt</h3>
-            <p className="text-left text-md whitespace-normal h-24 overflow-auto scrollbar">
-              OCR online application that can image-to-text conversion. If there was no
-              repository, I just screenshot, convert and copyMe. It is helpful sometimes.
+            <h3 className="text-center md:text-xl text-md py-4 font-bold">
+              imxt
+            </h3>
+            <p className="text-justify md:text-base text-sm whitespace-normal md:h-24 h-16 overflow-auto scrollbar">
+              Simple OCR online application that can image-to-text conversion.
+              You can use this whenever you want to copy text code.
             </p>
             <br />
             <div className="flex justify-between py-4">
@@ -104,14 +109,14 @@ function Projects({ textEnter, textLeave }) {
                 onClick={() =>
                   window.open("https://imxt.vercel.app/", "_blank")
                 }
-                className="cursor-pointer inline-flex items-center py-2 px-4 text-md font-medium text-center bg-[#0A1929] text-white hover:text-[#64FFDA] rounded-2xl hover:scale-105 hover:rounded-none duration-200 ease-in-out"
+                className="cursor-pointer inline-flex items-center py-2 px-4 md:text-sm text-xs font-medium text-center bg-[#0A1929] text-white hover:text-[#64FFDA] rounded-2xl hover:scale-105 hover:rounded-none duration-200 ease-in-out"
               >
                 LIVE
                 <BiRightArrow className="ml-2" />
               </div>
 
               <FaGithub
-                className="w-10 h-10 cursor-pointer hover:text-[#64FFDA] duration-200 ease-in-out"
+                className="md:w-10 md:h-10 w-7 h-7 cursor-pointer hover:text-[#64FFDA] duration-200 ease-in-out"
                 onClick={() =>
                   window.open("https://github.com/muhay007/imxt", "_blank")
                 }
@@ -119,7 +124,7 @@ function Projects({ textEnter, textLeave }) {
             </div>
           </div>
         </div>
-        <div className="bg-gray-400 rounded-2xl bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border-indigo-900 border-2 dark:border-none w-full">
+        <div className="bg-gray-400 rounded-2xl bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border-indigo-900 border-2 dark:border-none md:w-full w-5/6">
           <img
             src={tea}
             alt=""
@@ -146,32 +151,35 @@ function Projects({ textEnter, textLeave }) {
                 CHARTJS
               </span>
             </div>
-            <h3 className="text-center text-xl py-4 font-bold">tea101</h3>
-            <p className="text-left text-md whitespace-normal h-24 overflow-auto scrollbar">
-              <span class="dark:text-red-100 text-red-300">
+            <h3 className="text-center md:text-xl text-md py-4 font-bold">
+              tea101
+            </h3>
+            <p className="text-justify md:text-base text-sm whitespace-normal md:h-24 h-16 overflow-auto scrollbar">
+              <span className="dark:text-red-100 text-red-300">
                 Development of Supply Chain with Stock Monitoring for TEA101
               </span>
-              . This is my capstone project, am trying to improve it. Will be available soon..
+              . This is my capstone project, am trying to improve it. Will be
+              available soon..
             </p>
             <br />
             <div className="flex justify-between py-4">
               <div
                 onClick={() => window.open("http://tea101.rf.gd/", "_blank")}
-                className="cursor-pointer inline-flex items-center py-2 px-4 text-md font-medium text-center bg-[#0A1929] text-white hover:text-[#64FFDA] rounded-2xl hover:scale-105 hover:rounded-none duration-200 ease-in-out"
+                className="cursor-pointer inline-flex items-center py-2 px-4 md:text-sm text-xs font-medium text-center bg-[#0A1929] text-white hover:text-[#64FFDA] rounded-2xl hover:scale-105 hover:rounded-none duration-200 ease-in-out"
               >
                 LIVE
                 <BiRightArrow className="ml-2" />
               </div>
 
               <FaGithub
-                className="line-through w-10 h-10 cursor-pointer hover:text-[#64FFDA] duration-200 ease-in-out"
+                className=" md:w-10 md:h-10 w-7 h-7 cursor-pointer hover:text-[#64FFDA] duration-200 ease-in-out"
                 // onClick={() => window.open("", "_blank")}
               />
             </div>
           </div>
         </div>
 
-  <div className="bg-gray-400 rounded-2xl bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border-indigo-900 border-2 dark:border-none w-full">
+        <div className="bg-gray-400 rounded-2xl bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border-indigo-900 border-2 dark:border-none md:w-full w-5/6">
           <img
             src={brms}
             alt=""
@@ -191,34 +199,39 @@ function Projects({ textEnter, textLeave }) {
               <span className="bg-[#0A1929]  hover:text-[#64FFDA] text-xs font-semibold mr-2 px-2.5 py-0.5 rounded">
                 DATATABLES
               </span>
-        
             </div>
-            <h3 className="text-center text-xl py-4 font-bold">brms</h3>
-            <p className="text-left text-md whitespace-normal h-24 overflow-auto scrollbar">
-              <span class="dark:text-red-100 text-red-300">
+            <h3 className="text-center md:text-xl text-md py-4 font-bold">
+              brms
+            </h3>
+            <p className="text-justify md:text-base text-sm whitespace-normal md:h-24 h-16 overflow-auto scrollbar">
+              <span className="dark:text-red-100 text-red-300">
                 Book Record Management System
               </span>
-              .Thesis group project for QCU library, learned alot in this project like the basic crud.
+              . Thesis group project for QCU library. I learned the basic crud
+              using PHP procedural language and querying from MySQL xampp
+              database.
             </p>
             <br />
             <div className="flex justify-between py-4">
               <div
-                onClick={() => window.open("http://qculibrary.rf.gd/", "_blank")}
-                className="cursor-pointer inline-flex items-center py-2 px-4 text-md font-medium text-center bg-[#0A1929] text-white hover:text-[#64FFDA] rounded-2xl hover:scale-105 hover:rounded-none duration-200 ease-in-out"
+                onClick={() =>
+                  window.open("http://qculibrary.rf.gd/", "_blank")
+                }
+                className="cursor-pointer inline-flex items-center py-2 px-4 md:text-sm text-xs font-medium text-center bg-[#0A1929] text-white hover:text-[#64FFDA] rounded-2xl hover:scale-105 hover:rounded-none duration-200 ease-in-out"
               >
                 LIVE
                 <BiRightArrow className="ml-2" />
               </div>
 
               <FaGithub
-                className="line-through w-10 h-10 cursor-pointer hover:text-[#64FFDA] duration-200 ease-in-out"
+                className=" md:w-10 md:h-10 w-7 h-7 cursor-pointer hover:text-[#64FFDA] duration-200 ease-in-out"
                 // onClick={() => window.open("", "_blank")}
               />
             </div>
           </div>
         </div>
 
-        <div className="bg-gray-400 rounded-2xl bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border-indigo-900 border-2 dark:border-none w-full">
+        <div className="bg-gray-400 rounded-2xl bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border-indigo-900 border-2 dark:border-none md:w-full w-5/6">
           <img
             src={netlok}
             alt=""
@@ -242,11 +255,13 @@ function Projects({ textEnter, textLeave }) {
                 REACT_PLAYER
               </span>
             </div>
-            <h3 className="text-center text-xl py-4 font-bold">netlok</h3>
-            <p className="text-left text-md whitespace-normal h-24 overflow-auto scrollbar">
-              a movie web app that can view upcoming movies, popular etc., view
-              details, watch trailer. i tried combining 2 most popular which is
-              netflix and loklok theme. i used new version of
+            <h3 className="text-center md:text-xl text-md py-4 font-bold">
+              netlok
+            </h3>
+            <p className="text-justify md:text-base text-sm whitespace-normal md:h-24 h-16 overflow-auto scrollbar">
+              Movie web app that can view upcoming movies, popular etc., view
+              details, watch trailer. I tried combining 2 most popular which is
+              netflix and loklok theme. I used new version of
               react-router-dom-v6.
             </p>
             <br />
@@ -255,16 +270,63 @@ function Projects({ textEnter, textLeave }) {
                 onClick={() =>
                   window.open("https://netlok.vercel.app/", "_blank")
                 }
-                className="cursor-pointer inline-flex items-center py-2 px-4 text-md font-medium text-center bg-[#0A1929] text-white hover:text-[#64FFDA] rounded-2xl hover:scale-105 hover:rounded-none duration-200 ease-in-out"
+                className="cursor-pointer inline-flex items-center py-2 px-4 md:text-sm text-xs font-medium text-center bg-[#0A1929] text-white hover:text-[#64FFDA] rounded-2xl hover:scale-105 hover:rounded-none duration-200 ease-in-out"
               >
                 LIVE
                 <BiRightArrow className="ml-2" />
               </div>
 
               <FaGithub
-                className="w-10 h-10 cursor-pointer hover:text-[#64FFDA] duration-200 ease-in-out"
+                className="md:w-10 md:h-10 w-7 h-7 cursor-pointer hover:text-[#64FFDA] duration-200 ease-in-out"
                 onClick={() =>
                   window.open("https://github.com/muhay007/netlok", "_blank")
+                }
+              />
+            </div>
+          </div>
+        </div>
+        <div className="bg-gray-400 rounded-2xl bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border-indigo-900 border-2 dark:border-none md:w-full w-5/6">
+          <img
+            src={todos}
+            alt=""
+            className="object-fit w-full h-3/5 object-center py-4 px-6  duration-200 ease-linear"
+          />
+          <div className=" dark:text-white text-gray-700 w-4/5 mx-auto ">
+            <div className="flex text-white gap-2 items-center overflow-auto w-full py-2 scrollbar">
+              <span className="bg-[#0A1929]  hover:text-[#64FFDA] text-xs font-semibold mr-2 px-2.5 py-0.5 rounded">
+                REACT
+              </span>
+              <span className="bg-[#0A1929] hover:text-[#64FFDA] text-xs font-semibold mr-2 px-2.5 py-0.5 rounded">
+                TAILWINDCSS
+              </span>
+              <span className="bg-[#0A1929]  hover:text-[#64FFDA] text-xs font-semibold mr-2 px-2.5 py-0.5 rounded">
+                FIREBASE
+              </span>
+            </div>
+            <h3 className="text-center md:text-xl text-md py-4 font-bold">
+              todos
+            </h3>
+            <p className="text-justify md:text-base text-sm whitespace-normal md:h-24 h-16 overflow-auto scrollbar">
+              Simple todo app crud web application using reactjs, tailwind and
+              firebase database. I'm currently exploring and will use firebase
+              for my future projects.
+            </p>
+            <br />
+            <div className="flex justify-between py-4">
+              <div
+                onClick={() =>
+                  window.open("https://todos-orcin.vercel.app/", "_blank")
+                }
+                className="cursor-pointer inline-flex items-center py-2 px-4 md:text-sm text-xs font-medium text-center bg-[#0A1929] text-white hover:text-[#64FFDA] rounded-2xl hover:scale-105 hover:rounded-none duration-200 ease-in-out"
+              >
+                LIVE
+                <BiRightArrow className="ml-2" />
+              </div>
+
+              <FaGithub
+                className="md:w-10 md:h-10 w-7 h-7 cursor-pointer hover:text-[#64FFDA] duration-200 ease-in-out"
+                onClick={() =>
+                  window.open("https://github.com/muhay007/todos", "_blank")
                 }
               />
             </div>
