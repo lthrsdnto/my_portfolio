c09173546d88c28f5b0a8a5bf2137f3f1de4adbe
Luther Sedanto <109575711+muhay007@users.noreply.github.com>
2022-09-05 16:44:25 +0800

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -21,8 +21,8 @@ function Projects({ textEnter, textLeave }) {
           {"<"}Projects{"/>"}
         </motion.h1>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 place-items-center p-4 mx-auto">
-        <div className="bg-gray-400 rounded-2xl bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border-indigo-900 border-2 dark:border-none md:w-full w-5/6">
+      <div className="grid grid-cols-1 md:grid-cols-3 md:gap-8 gap-4 place-items-center p-4 mx-auto">
+        <div className="bg-gray-400 rounded-2xl bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 md:w-full w-5/6">
           <img
             src={splashy}
             alt=""
@@ -49,7 +49,7 @@ function Projects({ textEnter, textLeave }) {
             <h3 className="text-center md:text-xl text-md py-4 font-bold">
               splashy
             </h3>
-            <p className="text-justify md:text-base text-sm whitespace-normal md:h-24 h-16 overflow-auto scrollbar px-2">
+            <p className="text-justify md:text-base text-sm whitespace-normal md:h-24 h-16 overflow-auto scrollbar md:px-6 px-4">
               This project uses the Unsplash API to create an image search web
               app. Helped me understand how http requests, data retrieval, and
               showing them in the browser.
@@ -75,7 +75,7 @@ function Projects({ textEnter, textLeave }) {
             </div>
           </div>
         </div>
-        <div className="bg-gray-400 rounded-2xl bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border-indigo-900 border-2 dark:border-none md:w-full w-5/6">
+        <div className="bg-gray-400 rounded-2xl bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 md:w-full w-5/6">
           <img
             src={imxt}
             alt=""
@@ -99,7 +99,7 @@ function Projects({ textEnter, textLeave }) {
             <h3 className="text-center md:text-xl text-md py-4 font-bold">
               imxt
             </h3>
-            <p className="text-justify md:text-base text-sm whitespace-normal md:h-24 h-16 overflow-auto scrollbar">
+            <p className="text-justify md:text-base text-sm whitespace-normal md:h-24 h-16 overflow-auto scrollbar md:px-6 px-4">
               Simple OCR online application that can image-to-text conversion.
               You can use this whenever you want to copy text code.
             </p>
@@ -124,7 +124,7 @@ function Projects({ textEnter, textLeave }) {
             </div>
           </div>
         </div>
-        <div className="bg-gray-400 rounded-2xl bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border-indigo-900 border-2 dark:border-none md:w-full w-5/6">
+        <div className="bg-gray-400 rounded-2xl bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 md:w-full w-5/6">
           <img
             src={tea}
             alt=""
@@ -154,7 +154,7 @@ function Projects({ textEnter, textLeave }) {
             <h3 className="text-center md:text-xl text-md py-4 font-bold">
               tea101
             </h3>
-            <p className="text-justify md:text-base text-sm whitespace-normal md:h-24 h-16 overflow-auto scrollbar">
+            <p className="text-justify md:text-base text-sm whitespace-normal md:h-24 h-16 overflow-auto scrollbar md:px-6 px-4">
               <span className="dark:text-red-100 text-red-300">
                 Development of Supply Chain with Stock Monitoring for TEA101
               </span>
@@ -179,7 +179,7 @@ function Projects({ textEnter, textLeave }) {
           </div>
         </div>
 
-        <div className="bg-gray-400 rounded-2xl bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border-indigo-900 border-2 dark:border-none md:w-full w-5/6">
+        <div className="bg-gray-400 rounded-2xl bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 md:w-full w-5/6">
           <img
             src={brms}
             alt=""
@@ -203,7 +203,7 @@ function Projects({ textEnter, textLeave }) {
             <h3 className="text-center md:text-xl text-md py-4 font-bold">
               brms
             </h3>
-            <p className="text-justify md:text-base text-sm whitespace-normal md:h-24 h-16 overflow-auto scrollbar">
+            <p className="text-justify md:text-base text-sm whitespace-normal md:h-24 h-16 overflow-auto scrollbar md:px-6 px-4">
               <span className="dark:text-red-100 text-red-300">
                 Book Record Management System
               </span>
@@ -231,7 +231,7 @@ function Projects({ textEnter, textLeave }) {
           </div>
         </div>
 
-        <div className="bg-gray-400 rounded-2xl bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border-indigo-900 border-2 dark:border-none md:w-full w-5/6">
+        <div className="bg-gray-400 rounded-2xl bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 md:w-full w-5/6">
           <img
             src={netlok}
             alt=""
@@ -258,7 +258,7 @@ function Projects({ textEnter, textLeave }) {
             <h3 className="text-center md:text-xl text-md py-4 font-bold">
               netlok
             </h3>
-            <p className="text-justify md:text-base text-sm whitespace-normal md:h-24 h-16 overflow-auto scrollbar">
+            <p className="text-justify md:text-base text-sm whitespace-normal md:h-24 h-16 overflow-auto scrollbar md:px-6 px-4">
               Movie web app that can view upcoming movies, popular etc., view
               details, watch trailer. I tried combining 2 most popular which is
               netflix and loklok theme. I used new version of
@@ -285,7 +285,7 @@ function Projects({ textEnter, textLeave }) {
             </div>
           </div>
         </div>
-        <div className="bg-gray-400 rounded-2xl bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border-indigo-900 border-2 dark:border-none md:w-full w-5/6">
+        <div className="bg-gray-400 rounded-2xl bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 md:w-full w-5/6">
           <img
             src={todos}
             alt=""
@@ -306,7 +306,7 @@ function Projects({ textEnter, textLeave }) {
             <h3 className="text-center md:text-xl text-md py-4 font-bold">
               todos
             </h3>
-            <p className="text-justify md:text-base text-sm whitespace-normal md:h-24 h-16 overflow-auto scrollbar">
+            <p className="text-justify md:text-base text-sm whitespace-normal md:h-24 h-16 overflow-auto scrollbar md:px-6 px-4">
               Simple todo app crud web application using reactjs, tailwind and
               firebase database. I'm currently exploring and will use firebase
               for my future projects.
